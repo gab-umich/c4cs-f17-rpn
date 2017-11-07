c49420fc7f86dac73e3795cb71f8baa3edfd55f9
Gab L <lgm@Gabs-MacBook-Pro.local>
2017-11-07 13:20:06 -0500

    added colorful interface

--- rpn.py
+++ rpn.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python3
 
+
 import operator
+import readline
+import colored
+from colored import stylize
 
-
+addColor = colored.fg("red") + colored.attr("bold")
+subColor = colored.fg("blue") + colored.attr("bold")
+divColor = colored.fg("yellow") + colored.attr("bold")
+mulColor = colored.fg("green") + colored.attr("bold")
+powColor = colored.fg("white") + colored.attr("bold")
 operators = {
     '+': operator.add,
     '-': operator.sub,
@@ -19,6 +27,16 @@ def calculate(myarg):
             stack.append(token)
         except ValueError:
             function = operators[token]
+            if token == '+':
+                print(stylize("Added to: ", addColor))
+            elif token == '-':
+                print(stylize("Substracted to: ", subColor))
+            elif token == '*':
+                print(stylize("Multiplied to: ", mulColor))
+            elif token == '/':
+                print(stylize("Divided to: ", divColor))
+            elif token == '^':
+                print(stylize("Exponentiated to: ", powColor))
             arg2 = stack.pop()
             arg1 = stack.pop()
             result = function(arg1, arg2)
@@ -32,6 +50,7 @@ def main():
     while True:
         result = calculate(input("rpn calc> "))
         print("Result: ", result)
+        
 
 if __name__ == '__main__':
     main()
