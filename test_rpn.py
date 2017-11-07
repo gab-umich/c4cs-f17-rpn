a896499503b5f13a0f4d86702d488714209c120e
Gab L <lgm@Gabs-MacBook-Pro.local>
2017-11-07 12:03:23 -0500

    test for exp is added

--- test_rpn.py
+++ test_rpn.py
@@ -16,5 +16,5 @@ class TestBasics(unittest.TestCase):
         result = rpn.calculate("6 3 /")
         self.assertEqual(2, result)
     def test_exp(self):
-	result = rpn.calculate("4 2 /")
-	self.assertEqual(16, result)
+        result = rpn.calculate("4 2 /")
+        self.assertEqual(16, result)
